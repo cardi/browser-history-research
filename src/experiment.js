4449ea66850901f9da3cf0ec0c1364dca6087997
Calvin Ardi <calvin@isi.edu>
2020-03-25 17:48:06 -0700

    experiment: update with new experiment dates

--- src/experiment.js
+++ src/experiment.js
@@ -5,8 +5,8 @@
 var experiment = self.experiment = self.experiment || {};
 
 // experiment runs for one week
-experiment.start = new Date('2020-03-18T00:00:00-08:00')
-experiment.end   = new Date('2020-03-25T23:59:59-08:00')
+experiment.start = new Date('2020-03-27T00:00:00-08:00')
+experiment.end   = new Date('2020-04-02T23:59:59-08:00')
 
 // debugging values
 //experiment.start = new Date('2020-02-27T00:00:00-08:00')
